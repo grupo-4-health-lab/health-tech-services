f18eb3da14075618e77b5d10f799ca6df46fb54d
Edu Caribe <edu.caribe@hotmail.com>
2021-07-04 11:01:22 -0300

    feat: deploy config

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
   "production": {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
-    "database": "health_tech",
+    "database": process.env.DATABASE_NAME,
     "host": process.env.DATABASE_HOST,
     "dialect": "mysql"
   }
